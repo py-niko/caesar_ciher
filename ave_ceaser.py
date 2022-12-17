e03e01c71854cb8a8527597da712c7816dc7495b
Kirill N <nklv65@gmail.com>
2022-12-17 15:48:36 +0500

    ceaser_cipher

--- ave_ceaser.py
+++ ave_ceaser.py
@@ -1,26 +1,27 @@
 # На вход программе подается строка текста на английском языке, в которой нужно зашифровать все слова. Каждое слово строки следует зашифровать с помощью шифра
 # Цезаря (циклического сдвига на длину этого слова). Строчные буквы при этом остаются строчными, а прописные – прописными.
-s = input().split()
-result = []
-for i in s:
-    key = len(i)
-    result.append(' ')
-    for j in range(key):
-        if  i[j].isalpha() == True and i[j].isupper() == True:
-            if (ord(i[j]) + key) > 90:
-                result.append(chr(ord(i[j]) + key - 26))
-            else:
-                result.append(chr(ord(i[j]) + key))
+while True:
+    def ceaser_chipher(s):
+        result = []
+        for i in s:
+            key = len(i)
+            result.append(' ')
+            for j in range(key):
+                if  i[j].isalpha() == True and i[j].isupper() == True:
+                    if (ord(i[j]) + key) > 90:
+                        result.append(chr(ord(i[j]) + key - 26))
+                    else:
+                        result.append(chr(ord(i[j]) + key))
 
-        if i[j].isalpha() == True and i[j].islower() == True:
-            if (ord(i[j]) + key) > 122:
-                result.append(chr(ord(i[j]) + key - 26))
-            else:
-                result.append(chr(ord(i[j]) + key))
-del result[0]
-print(''.join(result))
+                if i[j].isalpha() == True and i[j].islower() == True:
+                    if (ord(i[j]) + key) > 122:
+                        result.append(chr(ord(i[j]) + key - 26))
+                    else:
+                        result.append(chr(ord(i[j]) + key))
+        del result[0]
+        print(''.join(result))
 
-
+    ceaser_chipher(input('Привет введите строку для шифра:\n').split())
 
 
 
